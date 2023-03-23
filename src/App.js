89ef227b9e791c5715c17a20be1c9b5e17332792
vaibhavpathak99 <pathakvaibhav6480@gmail.com>
2023-03-23 17:51:36 +0530

    login and logout validation

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import './App.css';
 import Home from './Home';
 import Login from './Login';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <Routes>
+        <Route
+          exact
+          path="home"
+          element={
+            localStorage.getItem("logged") == "true" ? (
+              <Home />
+            ) : (
+              <Navigate replace to={"/"} />
+            )
+          } />
+
         <Route path="*" element={<Login />}>
         </Route>
         <Route path="home" element={<Home />}>
